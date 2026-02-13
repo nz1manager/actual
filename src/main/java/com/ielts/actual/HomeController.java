fd635857665617e986538bc21f0dee02fb56f69d
nz1manager <nz1manager@gmail.com>
2026-02-14 00:24:07 +0500

    Added CrossOrigin to HomeController

--- src/main/java/com/ielts/actual/HomeController.java
+++ src/main/java/com/ielts/actual/HomeController.java
@@ -1,10 +1,13 @@
-package com.ielts.actual; // Papka nomiga qarab tekshirib oling
+package com.ielts.actual;
 
+import org.springframework.web.bind.annotation.CrossOrigin; // Mana buni qo'shish shart!
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*") // 1. Mana shu qatorni qo'shing
 @RestController
 public class HomeController {
+
     @GetMapping("/")
     public String home() {
         return "<h1>Server ishlayapti, Otabek!</h1><p>IELTS loyihasiga xush kelibsiz.</p>";
---
+++ src/main/java/com/ielts/actual/QuestionController.java
@@ -0,0 +1,16 @@
+package com.ielts.actual;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/questions") // Bu Controller'ning manzili
+@CrossOrigin(origins = "*")      // Frontend ulanishi uchun ruxsat
+public class QuestionController {
+
+    // Hozircha bazasiz, shunchaki test uchun ro'yxat qaytaramiz
+    @GetMapping
+    public List<String> getQuestions() {
+        return List.of("Describe a person you admire", "Talk about a traditional food");
+    }
+}
