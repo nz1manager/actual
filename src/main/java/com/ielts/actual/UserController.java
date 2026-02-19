a0a853840f633ef006c7a701996f65aa1d58289b
nz1manager <nz1manager@gmail.com>
2026-02-20 00:43:43 +0500

    Import ResponseEntity in UserController
    Added import for ResponseEntity in UserController.

--- src/main/java/com/ielts/actual/UserController.java
+++ src/main/java/com/ielts/actual/UserController.java
@@ -1,9 +1,11 @@
 package com.ielts.actual;
 
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
