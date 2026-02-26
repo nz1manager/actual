535e10b34db9467a040decdcad84431e79710eaf
nz1manager <nz1manager@gmail.com>
2026-02-26 23:42:36 +0500

    Update jwt.go

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,1 +1,44 @@
+package utils
 
+import (
+    "errors"
+    "os"
+    "time"
+    "github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+    UserID  string `json:"user_id"`
+    IsAdmin bool   `json:"is_admin"`
+    jwt.RegisteredClaims
+}
+
+func GenerateToken(userID string, isAdmin bool) (string, error) {
+    claims := Claims{
+        userID,
+        isAdmin,
+        jwt.RegisteredClaims{
+            ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour * 7)),
+            IssuedAt:  jwt.NewNumericDate(time.Now()),
+        },
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func ValidateToken(tokenString string) (*Claims, error) {
+    token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+        return []byte(os.Getenv("JWT_SECRET")), nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+        return claims, nil
+    }
+
+    return nil, errors.New("invalid token")
+}
