9df3052b0bd7dea7968c2ae3ba90779e0148ace2
nz1manager <nz1manager@gmail.com>
2026-02-26 23:42:51 +0500

    Update response.go

--- internal/utils/response.go
+++ internal/utils/response.go
@@ -1,1 +1,31 @@
+package utils
 
+import "github.com/gin-gonic/gin"
+
+type Response struct {
+    Success bool        `json:"success"`
+    Message string      `json:"message,omitempty"`
+    Data    interface{} `json:"data,omitempty"`
+    Error   string      `json:"error,omitempty"`
+}
+
+func SuccessResponse(c *gin.Context, status int, data interface{}) {
+    c.JSON(status, Response{
+        Success: true,
+        Data:    data,
+    })
+}
+
+func ErrorResponse(c *gin.Context, status int, message string) {
+    c.JSON(status, Response{
+        Success: false,
+        Error:   message,
+    })
+}
+
+func MessageResponse(c *gin.Context, status int, message string) {
+    c.JSON(status, Response{
+        Success: true,
+        Message: message,
+    })
+}
