af92d63e06fc63a2ee2155cce371e2f4ab50ccbf
nz1manager <nz1manager@gmail.com>
2026-02-26 23:38:28 +0500

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,31 @@
+package config
 
+import (
+    "log"
+    "os"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "gorm.io/gorm/logger"
+    "yourmodule/internal/models"
+)
+
+func InitDB() *gorm.DB {
+    dsn := os.Getenv("DATABASE_URL")
+    if dsn == "" {
+        log.Fatal("DATABASE_URL environment variable not set")
+    }
+
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+        Logger: logger.Default.LogMode(logger.Info),
+    })
+    if err != nil {
+        log.Fatal("Failed to connect to database:", err)
+    }
+
+    // Auto migrate schemas
+    if err := db.AutoMigrate(&models.User{}, &models.Test{}, &models.Submission{}); err != nil {
+        log.Fatal("Failed to migrate database:", err)
+    }
+
+    return db
+}
