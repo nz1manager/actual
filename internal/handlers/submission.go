61f1114e5b6598cd64700b2e2a3a3cb374a5c0bc
nz1manager <nz1manager@gmail.com>
2026-02-27 00:33:44 +0500

    Update submission.go

--- internal/handlers/submission.go
+++ internal/handlers/submission.go
@@ -1,1 +1,17 @@
+package handlers
 
+import (
+    "github.com/gin-gonic/gin"
+)
+
+type SubmissionHandler struct {
+    // repo fields here
+}
+
+func NewSubmissionHandler() *SubmissionHandler {
+    return &SubmissionHandler{}
+}
+
+func (h *SubmissionHandler) GetSubmission(c *gin.Context) {
+    c.JSON(200, gin.H{"message": "submission handler"})
+}
